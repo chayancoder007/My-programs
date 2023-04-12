365bfec0b39eeeb5c6943addf2bfe7285d558d2b
The Chayan <91484285+chayancoder007@users.noreply.github.com>
2023-04-12 18:55:40 +0530

    Add files via upload
    Nothing more just a simple code for addition on java

---
+++ addition.java
@@ -0,0 +1,12 @@
+package com.chayan;
+
+public class addition{
+    public static void main(String[] arg){
+        System.out.print("this is the addition of three numbers");
+        int num1 = 10;
+        int num2 =34;
+        int num3 = 56;
+        int sum = num1+num2+num3;
+        System.out.print(sum);
+    }
+}
